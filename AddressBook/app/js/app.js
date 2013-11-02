aae56102bddf8318d75bf60a23418f7fdcbc2fc4
jacqueline <jacq@huiskamer.com>
2013-11-02 15:05:37 +0100

    added Angular and Bootstrap

---
+++ AddressBook/app/js/app.js
@@ -0,0 +1,25 @@
+﻿//app/lib/app.js:
+var addressbookModule = angular.module('addressbookModule', []);
+
+addressbookModule.factory('personData', function ($http) {
+    return {
+        getPersons: function (successcb) {
+            $http({ method: 'GET', url: 'http://localhost:8000/api/person' })
+                    .success(function (data, status, headers, config) {
+                        successcb(data);
+                    })
+                    .error(function (data, status, headers, config) {
+                        $log.warn(data, status, headers, config);
+                    });
+            }
+    };
+});
+
+
+addressbookModule.controller('PersonController', function ($scope,personData) {
+    $scope.appname = "Awesome Addressbook";
+   
+    personData.getPersons(function (persons) {
+        $scope.persons = persons;
+    });
+});
